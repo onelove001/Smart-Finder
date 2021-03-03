ab15f8b432d7e06521c852f222669204fa5a61b1
Derick Johnson <efissy@Dericks-MacBook-Pro.local>
2021-03-04 00:49:10 +0100

    messag

--- chat/admin.py
+++ chat/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 
 
-from .models import Thread, ChatMessage
+# from .models import Thread, ChatMessage
 
 class ChatMessage(admin.TabularInline):
-    model = ChatMessage
+    pass
+    # model = ChatMessage
 
 class ThreadAdmin(admin.ModelAdmin):
-    inlines = [ChatMessage]
-    class Meta:
-        model = Thread 
+    pass
+    # inlines = [ChatMessage]
+    # class Meta:
+    #     model = Thread 
 
 
-admin.site.register(Thread, ThreadAdmin)
+# admin.site.register(Thread, ThreadAdmin)
